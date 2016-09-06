6b482b65d9b294cf2e44e338d5305917d2698326
Reconix <reconix@outlook.com>
2016-09-06 23:55:35 +0100

    Autoload cleanup
    Added missing Drivers and cleanup.

--- ForgeIgniter/config/autoload.php
+++ ForgeIgniter/config/autoload.php
@@ -1,4 +1,4 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 /*
 | -------------------------------------------------------------------
 | AUTO-LOADER
@@ -20,10 +20,11 @@
 |
 | 1. Packages
 | 2. Libraries
-| 3. Helper files
-| 4. Custom config files
-| 5. Language files
-| 6. Models
+| 3. Drivers
+| 4. Helper files
+| 5. Custom config files
+| 6. Language files
+| 7. Models
 |
 */
 
@@ -36,24 +37,47 @@
 |  $autoload['packages'] = array(APPPATH.'third_party', '/usr/local/shared');
 |
 */
-
 $autoload['packages'] = array(APPPATH.'third_party');
-
 
 /*
 | -------------------------------------------------------------------
 |  Auto-load Libraries
 | -------------------------------------------------------------------
-| These are the classes located in the system/libraries folder
-| or in your application/libraries folder.
+| These are the classes located in system/libraries/ or your
+| application/libraries/ directory, with the addition of the
+| 'database' library, which is somewhat of a special case.
 |
 | Prototype:
 |
-|	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
+|	$autoload['libraries'] = array('database', 'email', 'session');
+|
+| You can also supply an alternative library name to be assigned
+| in the controller:
+|
+|	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-
 $autoload['libraries'] = array('database', 'session', 'site', 'core', 'uploads');
 
+/*
+| -------------------------------------------------------------------
+|  Auto-load Drivers
+| -------------------------------------------------------------------
+| These classes are located in system/libraries/ or in your
+| application/libraries/ directory, but are also placed inside their
+| own subdirectory and they extend the CI_Driver_Library class. They
+| offer multiple interchangeable driver options.
+|
+| Prototype:
+|
+|	$autoload['drivers'] = array('cache');
+|
+| You can also supply an alternative property name to be assigned in
+| the controller:
+|
+|	$autoload['drivers'] = array('cache' => 'cch');
+|
+*/
+$autoload['drivers'] = array();
 
 /*
 | -------------------------------------------------------------------
@@ -63,9 +87,7 @@ $autoload['libraries'] = array('database', 'session', 'site', 'core', 'uploads')
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-
-$autoload['helper'] = array('url', 'form', 'date', 'halogy', 'cookie');
-
+$autoload['helper'] = array('url', 'form', 'date', 'halogy', 'cookie', 'language');
 
 /*
 | -------------------------------------------------------------------
@@ -76,12 +98,10 @@ $autoload['helper'] = array('url', 'form', 'date', 'halogy', 'cookie');
 |	$autoload['config'] = array('config1', 'config2');
 |
 | NOTE: This item is intended for use ONLY if you have created custom
-| config files.  Otherwise, leave it blank.
+| config files. Otherwise, leave it blank.
 |
 */
-
 $autoload['config'] = array('site_config');
-
 
 /*
 | -------------------------------------------------------------------
@@ -95,9 +115,7 @@ $autoload['config'] = array('site_config');
 | "codeigniter_lang.php" would be referenced as array('codeigniter');
 |
 */
-
 $autoload['language'] = array();
-
 
 /*
 | -------------------------------------------------------------------
@@ -105,12 +123,11 @@ $autoload['language'] = array();
 | -------------------------------------------------------------------
 | Prototype:
 |
-|	$autoload['model'] = array('model1', 'model2');
+|	$autoload['model'] = array('first_model', 'second_model');
 |
+| You can also supply an alternative model name to be assigned
+| in the controller:
+|
+|	$autoload['model'] = array('first_model' => 'first');
 */
-
 $autoload['model'] = array();
-
-
-/* End of file autoload.php */
-/* Location: ./ForgeIgniter/config/autoload.php */
